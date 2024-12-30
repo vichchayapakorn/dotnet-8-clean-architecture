d9a1afc93d181099bef1ac6b8dadba0de4c5cdce
Holohay <holohay.dk@gmail.com>
2024-12-30 16:01:21 +0700

    commit

--- dotnet-8-clean-architecture/obj/Debug/net8.0/dotnet-8-clean-architecture.AssemblyInfo.cs
+++ dotnet-8-clean-architecture/obj/Debug/net8.0/dotnet-8-clean-architecture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-8-clean-architecture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e859f8e260ce7afb34ab9b8660891c730bc4855d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-8-clean-architecture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-8-clean-architecture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
